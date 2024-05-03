dffea102c80ce4ca6e28a2e13dea350bab5355e9
Meghna Pamula <mpamula123@gmail.com>
2024-05-02 19:56:45 -0700

    push

---
+++ load_parallel.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+files=$(find data/*)
+
+echo '================================================================================'
+echo 'load tweets'
+echo '================================================================================'
+echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:80010/ --inputs
